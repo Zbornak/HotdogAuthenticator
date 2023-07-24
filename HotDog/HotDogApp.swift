985f764ddceba62f9d8e1fddd780d2ef3a756cfd
Mark <zbornack@protonmail.com>
2023-07-24 10:15:22 +0100

    Initial Commit

---
+++ HotDog/HotDogApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotDogApp.swift
+//  HotDog
+//
+//  Created by Mark Strijdom on 24/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct HotDogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
