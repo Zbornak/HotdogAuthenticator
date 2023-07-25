de183bff8b7757d5cbaeef2b47c7fd35f2a52dde
Mark <zbornack@protonmail.com>
2023-07-25 15:05:57 +0100

    Finished ImagePicker struct

--- HotDog/ImagePicker.swift
+++ HotDog/ImagePicker.swift
@@ -10,17 +10,35 @@ import SwiftUI
 
 struct ImagePicker: UIViewControllerRepresentable {
     class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        var parent: ImagePicker
         
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+            
+            guard let provider = results.first?.itemProvider else {
+                return
+            }
+            
+            if provider.canLoadObject(ofClass: UIImage.self) {
+                provider.loadObject(ofClass: UIImage.self) { image, _ in
+                    self.parent.image = image as? UIImage
+                }
+            }
+        }
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
     }
+    
+    @Binding var image: UIImage?
     
     func makeUIViewController(context: Context) -> PHPickerViewController {
         var config = PHPickerConfiguration()
         config.filter = .images
         
         let picker = PHPickerViewController(configuration: config)
-        
         picker.delegate = context.coordinator
-        
         return picker
     }
     
@@ -31,6 +49,6 @@ struct ImagePicker: UIViewControllerRepresentable {
     typealias UIViewControllerType = PHPickerViewController
     
     func makeCoordinator() -> Coordinator {
-        Coordinator()
+        Coordinator(self)
     }
 }
